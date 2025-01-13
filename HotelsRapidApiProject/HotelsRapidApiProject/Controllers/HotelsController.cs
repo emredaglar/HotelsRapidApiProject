2b5cd3bff14a8494dd43137442c207912de3ce88
emredaglar <emre.daglar1@gmail.com>
2025-01-13 16:38:39 +0300

    added BaseController

---
+++ HotelsRapidApiProject/HotelsRapidApiProject/Controllers/HotelsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelsRapidApiProject.Controllers
+{
+	public class HotelsController : Controller
+	{
+		private readonly HttpClient _httpClient;
+
+		public HotelsController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		private readonly string _apiKey = "3db79cb1e3mshfa9b8dd84816013p1e663fjsn48c308702bf9";
+		private readonly string _apiHost = "booking-com15.p.rapidapi.com";
+
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
